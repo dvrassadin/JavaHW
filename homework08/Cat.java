353e35be4f2e44e4fd2452467b663f2ffa56d4a1
Daniil Rassadin <daniil@rassadin.org>
2023-04-18 16:08:29 +0600

    The eighth homework's original files added

---
+++ homework08/Cat.java
@@ -0,0 +1,14 @@
+package homework08;
+
+public class Cat {
+    private String name;
+    private int appetite;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+
+    public void eat() {
+    }
+}
---
+++ homework08/Plate.java
@@ -0,0 +1,13 @@
+package homework08;
+
+public class Plate {
+    private int food;
+
+    public Plate(int food) {
+        this.food = food;
+    }
+
+    public void info() {
+        System.out.println("plate: " + food);
+    }
+}
