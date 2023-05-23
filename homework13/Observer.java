5a59a3d8d2c0c947d45cccc1d0d18f85b150bacf
Daniil Rassadin <daniil@rassadin.org>
2023-05-23 20:41:58 +0600

    Original files for homework 13

---
+++ homework13/Observer.java
@@ -0,0 +1,10 @@
+package homework13;
+
+/**
+ * Интерфейс наблюдателя (соискателя, человек в поисках работы)
+ */
+public interface Observer {
+
+    void receiveOffer(String companyName, double salary);
+
+}
---
+++ homework13/Publisher.java
@@ -0,0 +1,11 @@
+package homework13;
+
+public interface Publisher {
+
+    void registerObserver(Observer observer);
+
+    void removeObserver(Observer observer);
+
+    void sendOffer(String nameCompany, double salary);
+
+}
